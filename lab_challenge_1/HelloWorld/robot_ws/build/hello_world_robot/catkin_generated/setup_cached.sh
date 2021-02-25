b40031251315e3409486c1afefa1c11e36e6100e
nickTulip95 <nicktulip95@gmail.com>
2021-02-25 13:24:42 -0500

    Lab Challenge 1 Completed

---
+++ lab_challenge_1/HelloWorld/robot_ws/build/hello_world_robot/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/environment/HelloWorld/robot_ws/build/hello_world_robot/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ubuntu/environment/HelloWorld/robot_ws/build/hello_world_robot/devel/lib:/home/ubuntu/environment/HelloWorld/robot_ws/install/turtlebot3_description_reduced_mesh/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/melodic/bin:/home/ubuntu/.nvm/versions/node/v8.17.0/bin:/usr/local/rvm/gems/ruby-2.4.1/bin:/usr/local/rvm/gems/ruby-2.4.1@global/bin:/usr/local/rvm/rubies/ruby-2.4.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.local/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/home/ubuntu/bin:/usr/local/rvm/bin:/home/ubuntu/.local/bin:/home/ubuntu/bin'
+export PKG_CONFIG_PATH='/home/ubuntu/environment/HelloWorld/robot_ws/build/hello_world_robot/devel/lib/pkgconfig:/home/ubuntu/environment/HelloWorld/robot_ws/install/turtlebot3_description_reduced_mesh/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig'
+export PYTHONPATH="/home/ubuntu/environment/HelloWorld/robot_ws/build/hello_world_robot/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/environment/HelloWorld/robot_ws/build/hello_world_robot/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ubuntu/environment/HelloWorld/robot_ws/src/hello_world_robot:$ROS_PACKAGE_PATH"
